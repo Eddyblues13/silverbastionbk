ad669c869787b7b6645e09e07980ba29ca541d43
blueswayne133 <blueswayne133@gmail.com>
2025-09-17 02:17:29 -0700

    Add feature X

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -360,18 +360,6 @@ function googleTranslateElementInit() {
 
 <script type="text/javascript" src="//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit"></script>
 
-<!-- Smartsupp Live Chat script -->
-<script type="text/javascript">
-var _smartsupp = _smartsupp || {};
-_smartsupp.key = '49200d6735f1cc1d98223e08be5f1b31551fe3ee';
-window.smartsupp||(function(d) {
-  var s,c,o=smartsupp=function(){ o._.push(arguments)};o._=[];
-  s=d.getElementsByTagName('script')[0];c=d.createElement('script');
-  c.type='text/javascript';c.charset='utf-8';c.async=true;
-  c.src='https://www.smartsuppchat.com/loader.js?';s.parentNode.insertBefore(c,s);
-})(document);
-</script>
-<noscript> Powered by <a href=“https://www.smartsupp.com” target=“_blank”>Smartsupp</a></noscript>
 
   </body>
   

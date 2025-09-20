91f0dc5de8a6254380d826e1ef9101a63d038187
blueswayne133 <blueswayne133@gmail.com>
2025-09-20 08:12:30 -0700

    Add feature X

--- resources/views/layouts/dash2.blade.php
+++ resources/views/layouts/dash2.blade.php
@@ -327,7 +327,7 @@
 
     <div class="gtranslate_wrapper"></div>
     <script>
-        window.gtranslateSettings = {"default_language":"en","detect_browser_language":true,"wrapper_selector":".gtranslate_wrapper","switcher_horizontal_position":"left","switcher_vertical_position":"bottom","alt_flags":{"en":"usa","pt":"brazil","es":"colombia","fr":"quebec"}}
+        window.gtranslateSettings = {"default_language":"en","detect_browser_language":true,"wrapper_selector":".gtranslate_wrapper","switcher_horizontal_position":"right","switcher_vertical_position":"top","alt_flags":{"en":"usa","pt":"brazil","es":"colombia","fr":"quebec"}}
     </script>
     <script src="https://cdn.gtranslate.net/widgets/latest/float.js" defer></script>
 
